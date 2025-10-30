c5f6ce501e6f81e21b1230e829237edb2c57aefe
SadoChad23 <wildan0720@gmail.com>
2025-10-30 16:00:53 +0700

    validasi dan alert

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -3,3 +3,19 @@ document.getElementById("menuToggle").addEventListener("click", function() {
 });
 let nama = prompt("Siapa nama kamu?");
 alert("Halo, " + nama + "Cihuyy!");
+
+
+
+
+
+document.querySelector("form").addEventListener("submit", function (e) {
+const nama = document.getElementById("txtNama").value.trim();
+const email = document.getElementById("txtEmail").value.trim();
+const pesan = document.getElementById("txtPesan").value.trim();
+if (nama === "" || email === "" || pesan === "") {
+alert("Semua kolom wajib diisi!");
+e.preventDefault();
+} else {
+alert("Terima kasih, " + nama + "! Pesan Anda telah dikirim.");
+}
+});
